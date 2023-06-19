761cc8a17f27e3617c305574ece9566626520656
cristianoalves12 <cristianoalf012@gmail.com>
2023-06-18 23:33:53 -0300

    responsiveness and data capture

---
+++ script.js
@@ -0,0 +1,25 @@
+let btnAction = document.querySelector("#btn-action");
+let formDados = document.querySelector("#form-dados");
+let result = document.querySelector(".result");
+
+btnAction.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let formValues = new FormData(formDados);
+    let data = Object.fromEntries(formValues);
+    result.textContent="QUALIDADE";
+
+    fetch('https://reqres.in/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json()).then(data => console.log(data))
+
+
+
+    // let formDados = document.querySelector("#form-dados");
+    // console.log(formDados.fixedAcidity.value);
+
+})
